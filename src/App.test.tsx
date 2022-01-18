513406f7a9bdcd6275a8734ae4d4281fb768e373
Dennis Okparaocha <dennis_okpa@hotmail.co.uk>
2022-01-18 18:40:41 +0000

    Finished assignment

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,53 +1,68 @@
-import { screen, render } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import { App } from "./App";
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { App } from './App'
 
 const stateFixture = {
-  newTask: "eat the frog 20pts",
+  newTask: 'eat the frog 20pts',
   tasks: [
-    { name: "kill bill", points: 6 },
-    { name: "get shorty", points: 12 },
+    { name: 'kill bill', points: 6 },
+    { name: 'get shorty', points: 12 },
   ],
-};
+}
 
-describe("App", () => {
-  it("renders the task names", () => {
-    render(<App {...stateFixture} />);
+describe('App', () => {
+  it('renders the task names', () => {
+    render(<App {...stateFixture} />)
 
-    expect(screen.getByText("kill bill")).toBeInTheDocument();
-    expect(screen.getByText("get shorty")).toBeInTheDocument();
-  });
+    expect(screen.getByText('kill bill')).toBeInTheDocument()
+    expect(screen.getByText('get shorty')).toBeInTheDocument()
+  })
 
-  it("sorts tasks by descending point value", () => {
-    render(<App {...stateFixture} />);
+  it('sorts tasks by descending point value', () => {
+    render(<App {...stateFixture} />)
 
-    const tasks = screen.getAllByLabelText("task");
-    expect(tasks[0]).toHaveTextContent("get shorty");
-    expect(tasks[1]).toHaveTextContent("kill bill");
-  });
+    const tasks = screen.getAllByLabelText('task')
+    expect(tasks[0]).toHaveTextContent('get shorty')
+    expect(tasks[1]).toHaveTextContent('kill bill')
+  })
 
-  it("renders the correct class for the point threshold", () => {
-    render(<App {...stateFixture} />);
+  it('renders the correct class for the point threshold', () => {
+    render(<App {...stateFixture} />)
 
-    const normalTask = screen.getByText("kill bill");
-    const criticalTask = screen.getByText("get shorty");
+    const normalTask = screen.getByText('kill bill')
+    const criticalTask = screen.getByText('get shorty')
 
-    expect(normalTask).toHaveAttribute("aria-label", "normal");
-    expect(criticalTask).toHaveAttribute("aria-label", "critical");
-  });
+    expect(normalTask).toHaveAttribute('aria-label', 'normal')
+    expect(criticalTask).toHaveAttribute('aria-label', 'critical')
+  })
 
-  it("parses point input in the task name", () => {
-    render(<App {...stateFixture} />);
+  it('parses point input in the task name', () => {
+    render(<App {...stateFixture} />)
 
-    const existingTasks = screen.getAllByLabelText("task");
-    expect(existingTasks.length).toEqual(2);
+    const existingTasks = screen.getAllByLabelText('task')
+    expect(existingTasks.length).toEqual(2)
 
-    const submitButton = screen.getByLabelText("Add");
+    const submitButton = screen.getByLabelText('Add')
 
-    userEvent.click(submitButton);
+    userEvent.click(submitButton)
 
-    const tasks = screen.getAllByLabelText("task");
-    expect(tasks.length).toEqual(3);
-    expect(tasks[0]).toHaveTextContent("eat the frog");
-  });
-});
+    const tasks = screen.getAllByLabelText('task')
+    expect(tasks.length).toEqual(3)
+    expect(tasks[0]).toHaveTextContent('eat the frog')
+  })
+
+  it('should be able to update points', () => {
+    render(<App {...stateFixture} />)
+
+    const existingPoints = screen.getByText('12')
+    expect(existingPoints).toBeInTheDocument()
+
+    userEvent.click(existingPoints)
+    userEvent.type(existingPoints, '{backspace}{backspace}20')
+
+    userEvent.click(document.body)
+
+    const updatedPoints = screen.getByText('20')
+    expect(updatedPoints).toBeInTheDocument()
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useReducer } from 'react'
 import {
   Heading,
   Container,
@@ -12,53 +12,116 @@ import {
   Text,
   theme,
   Tag,
-} from "@chakra-ui/react";
-import { AddIcon, DeleteIcon } from "@chakra-ui/icons";
+} from '@chakra-ui/react'
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons'
 
-type TTask = string;
+type TTask = string
 
 type TTasks = {
-  name: string;
-  points: number;
-}[];
+  name: string
+  points: number
+}[]
 
 type TAppProps = {
-  newTask?: TTask;
-  tasks?: TTasks;
-};
+  newTask?: TTask
+  tasks?: TTasks
+}
 
 const taskStyles = {
   normal: {
     ListItem: {
-      borderBottomWidth: "1px",
-      borderBottomColor: "gray.200",
+      borderBottomWidth: '1px',
+      borderBottomColor: 'gray.200',
     },
     Tag: {
-      color: "gray.800",
-      background: "gray.200",
+      color: 'gray.800',
+      background: 'gray.200',
     },
     Text: {
-      "aria-label": "normal",
-      color: "gray.800",
+      'aria-label': 'normal',
+      color: 'gray.800',
     },
     IconButton: {
-      color: "gray.800",
-      background: "gray.200",
+      color: 'gray.800',
+      background: 'gray.200',
     },
   },
-};
+}
 
-export const App = ({ newTask = "", tasks: initialTasks = [] }: TAppProps) => {
-  const [task, setTask] = useState(newTask);
-  const [tasks, setTasks] = useState(initialTasks);
+const TASK_REGEX = new RegExp('(.*?) ([0-9]+)pts')
 
-  const addTask = () => setTasks([...tasks, { name: task, points: 0 }]);
+const processTask = (task: string) => {
+  let points = 0
+  let name = task
 
-  const removeTask = (i: number) => {
-    const newTasks = [...tasks];
-    newTasks.splice(i, 1);
-    setTasks(newTasks);
-  };
+  const scan: RegExpExecArray | null = TASK_REGEX.exec(task)
+
+  if (scan?.index !== -1 && scan?.[2]) {
+    points = Number(scan[2])
+    name = scan[1]
+  }
+
+  return { name, points }
+}
+
+interface State {
+  name: string
+  points: number
+}
+
+interface ModifiedState {
+  points: number
+  index: number
+}
+
+enum ActionType {
+  setTask,
+  removeTask,
+  updatePoints,
+}
+
+export type Action =
+  | {
+      type: ActionType.setTask
+      task: string
+    }
+  | {
+      type: ActionType.removeTask
+      task: number
+    }
+  | {
+      type: ActionType.updatePoints
+      task: ModifiedState
+    }
+
+const reducer = (state: State[], action: Action) => {
+  switch (action.type) {
+    case ActionType.setTask:
+      return [...state, processTask(action.task)]
+    case ActionType.removeTask:
+      return sortTasks(state).filter((_, index) => index !== action.task)
+    case ActionType.updatePoints:
+      return sortTasks(state).map((task, index) => {
+        if (index === action.task.index) {
+          return {
+            ...task,
+            points: action.task.points,
+          }
+        }
+        return task
+      })
+  }
+  return state
+}
+
+const sortTasks = (tasks: State[]) => tasks.sort((a, b) => b.points - a.points)
+
+const initialEditState = { index: -1, points: 0 }
+
+export const App = ({ newTask = '', tasks: initialTasks = [] }: TAppProps) => {
+  const [task, setTask] = useState(newTask)
+  const [edit, setEdit] = useState(initialEditState)
+  const [tasks, dispatch] = useReducer(reducer, initialTasks)
 
   return (
     <ChakraProvider theme={theme}>
@@ -68,10 +131,14 @@ export const App = ({ newTask = "", tasks: initialTasks = [] }: TAppProps) => {
       <Container>
         <Flex gap={2} mb={5}>
           <Input placeholder="Name" onChange={(e) => setTask(e.target.value)} />
-          <IconButton aria-label="Add" icon={<AddIcon />} onClick={addTask} />
+          <IconButton
+            aria-label="Add"
+            icon={<AddIcon />}
+            onClick={() => dispatch({ type: ActionType.setTask, task })}
+          />
         </Flex>
         <List borderTopWidth="1px" borderTopColor="gray.200">
-          {tasks.map((task, i) => (
+          {sortTasks(tasks).map((task, i) => (
             <ListItem
               key={i}
               aria-label="task"
@@ -79,14 +146,53 @@ export const App = ({ newTask = "", tasks: initialTasks = [] }: TAppProps) => {
               {...taskStyles.normal.ListItem}
             >
               <Flex justify="space-between" align="center">
-                <Tag aria-label="points" {...taskStyles.normal.Tag}>
-                  {task.points}
-                </Tag>{" "}
-                <Text {...taskStyles.normal.Text}>{task.name}</Text>
+                <Tag
+                  aria-label="points"
+                  {...taskStyles.normal.Tag}
+                  onClick={() =>
+                    setEdit({
+                      index: i,
+                      points: task.points,
+                    })
+                  }
+                >
+                  {edit.index === i ? (
+                    <input
+                      autoFocus
+                      onChange={(e) =>
+                        setEdit((prev) => ({
+                          ...prev,
+                          points: Number(e.target.value),
+                        }))
+                      }
+                      onBlur={() => {
+                        dispatch({ type: ActionType.updatePoints, task: edit })
+                        setEdit(initialEditState)
+                      }}
+                      value={edit.points}
+                    />
+                  ) : (
+                    task.points
+                  )}
+                </Tag>{' '}
+                <Text
+                  {...taskStyles.normal.Text}
+                  {...(task.points >= 10 && { 'aria-label': 'critical' })}
+                  onClick={() =>
+                    setEdit({
+                      index: i,
+                      points: task.points,
+                    })
+                  }
+                >
+                  {task.name}
+                </Text>
                 <IconButton
                   aria-label="Remove"
                   icon={<DeleteIcon />}
-                  onClick={() => removeTask(i)}
+                  onClick={() =>
+                    dispatch({ type: ActionType.removeTask, task: i })
+                  }
                 />
               </Flex>
             </ListItem>
@@ -94,5 +200,5 @@ export const App = ({ newTask = "", tasks: initialTasks = [] }: TAppProps) => {
         </List>
       </Container>
     </ChakraProvider>
-  );
-};
+  )
+}
